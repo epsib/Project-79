5127e6c2f0f099ee4bf28aa56466e72521776f00
Epsi Bhattacharya <epsibhattacharya256@gmail.com>
2021-06-08 22:25:46 -0400

    Package

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants'
+import DailyPic from './screens/DailyPic';
+import SpaceCrafts from './screens/SpaceCrafts';
+import StarMap from './screens/StarMap';
+import Home from './screens/Home'
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native'
+import {createStackNavigator} from '@react-navigation/stack';
 
+const Stack = createStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown:false}}>
+         <Stack.Screen name="Home" component={Home}/>
+         <Stack.Screen name="SpaceCrafts" component={SpaceCrafts}/>
+         <Stack.Screen name="DailyPic" component={DailyPic}/>
+         <Stack.Screen name="StarMap" component={StarMap}/>
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
