1d85b65ea413a3a9832b2bb45e2c06b871aa7145
Epsi Bhattacharya <epsibhattacharya256@gmail.com>
2021-06-08 22:26:12 -0400

    screens

---
+++ screens/DailyPic.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+class DailyPic extends React.Component{
+    render(){
+        return(
+            <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+                <Text>Daily Pic</Text>
+            </View>
+        )
+    }
+}
+
+export default DailyPic;
---
+++ screens/StarMap.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+class StarMap extends React.Component{
+    render(){
+        return(
+            <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+                <Text>Star Map</Text>
+            </View>
+        )
+    }
+}
+
+export default StarMap;
